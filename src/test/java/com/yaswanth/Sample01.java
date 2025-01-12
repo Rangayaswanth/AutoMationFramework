b671c096a00edefa652265efe0fbe8f0e222b8cd
rangayaswanth <yaswanthranga88@gmail.com>
2025-01-11 22:43:57 -0600

    Initial commit

---
+++ src/test/java/com/yaswanth/Sample01.java
@@ -0,0 +1,19 @@
+package com.yaswanth;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Sample01 {
+
+    @Test
+    public void sample001(){
+        Assert.assertTrue(true);
+
+    }
+
+
+
+
+
+
+}
